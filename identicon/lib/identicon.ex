38e5c5493d17b25e7f51a7f1bea163771e488b12
Tarciso Torres <tarcisoftorres@gmail.com>
2018-05-22 19:44:47 -0300

    Add method build_grid and mirror_row to identicon.ex and add attribute color in image.ex

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -3,12 +3,11 @@ defmodule Identicon do
    input
    |> hash_input
    |> pick_color
+   |> build_grid
  end
 
- def pick_color(image) do
-   %Identicon.Image{hex: hex_list} = image
-   [r, g, b | _tail] = hex_list
-   [r, g, b]
+ def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+  %Identicon.Image{image | color: {r, g, b}}
  end
 
  def hash_input(input) do
@@ -16,5 +15,18 @@ defmodule Identicon do
    |> :binary.bin_to_list
 
    %Identicon.Image{hex: hex}
+ end
+
+ def build_grid(%Identicon.Image{hex: hex} = image) do
+   hex
+   |> Enum.chunk(3)
+ end
+
+ def mirror_row(row) do
+   # [145, 46, 200]
+   [first, second | _tail] = row
+
+   # [145, 46, 200, 46, 145]
+   row ++ [second, first]
  end
 end
