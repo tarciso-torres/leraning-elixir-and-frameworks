995797e3c74dd2905899836308377b77272096fa
Tarciso Torres <tarciso.torres@ifactory.com.br>
2018-05-24 08:22:07 -0300

    Start project average_calculator

---
+++ average_calculator/lib/average_calculator.ex
@@ -0,0 +1,18 @@
+defmodule AverageCalculator do
+  @moduledoc """
+  Documentation for AverageCalculator.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AverageCalculator.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
