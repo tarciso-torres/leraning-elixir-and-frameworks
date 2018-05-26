bc82da599953eb76967000b29a13c2497ef38a95
Tarciso Torres <tarcisoftorres@gmail.com>
2018-05-25 23:40:38 -0300

    Add method build_pixel_map

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -5,6 +5,22 @@ defmodule Identicon do
    |> pick_color
    |> build_grid
    |> filter_odd_squares
+   |> build_pixel_map
+ end
+
+ def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+   pixel_map = Enum.map grid, fn({_code, index}) ->
+    horizontal = rem(index, 5) * 50
+    vertical = div(index, 5) * 50
+
+    top_left = {horizontal, vertical}
+    button_right = {horizontal + 50, vertical + 50}
+
+    {top_left, button_right}
+   end
+
+   %Identicon.Image{image | pixel_map: pixel_map}
+
  end
 
  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
