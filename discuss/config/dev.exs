69d46a45b027d50096f19a9756d1b4c442d2d0c3
Tarciso Torres <tarcisoftorres@gmail.com>
2018-05-28 19:10:24 -0300

    Create project ping_pong

--- discuss/config/dev.exs
+++ discuss/config/dev.exs
@@ -37,7 +37,7 @@ config :phoenix, :stacktrace_depth, 20
 config :discuss, Discuss.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "postgres",
+  password: "1234",
   database: "discuss_dev",
   hostname: "localhost",
   pool_size: 10
