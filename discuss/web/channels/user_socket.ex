12eb88637d95ab5e5b4bb2d770f0038939d662d6
Tarciso Torres <tarciso.torres@ifactory.com.br>
2018-07-20 09:49:11 -0300

    Create token in front-end

--- discuss/web/channels/user_socket.ex
+++ discuss/web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule Discuss.UserSocket do
 
   transport :websocket, Phoenix.Transports.WebSocket
 
-  def connect(_params, socket) do
+  def connect(%{"token" => token}, socket) do
+    IO.puts token
     {:ok, socket}
   end
 
